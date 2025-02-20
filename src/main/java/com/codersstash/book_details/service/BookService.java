51f2f32be4ac45eef5f077762c96b12063226194
codersstash <codersstashgitrepo@gmail.com>
2025-02-20 13:56:28 +0530

    BookService class functions.

--- src/main/java/com/codersstash/book_details/service/BookService.java
+++ src/main/java/com/codersstash/book_details/service/BookService.java
@@ -1,11 +1,26 @@
 package com.codersstash.book_details.service;
 
+import com.codersstash.book_details.entity.Book;
 import com.codersstash.book_details.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BookService {
     @Autowired
-    private BookRepository repository;
+    private BookRepository bookRepository;
+    public Book saveBook(Book book){
+        return bookRepository.save(book);
+    }
+    public Book getBookById(int book_id){
+        return bookRepository.findById(book_id);
+    }
+    public List<Book> getAllBooks(){
+        return bookRepository.findAll();
+    }
+    public void deleteBook(int id){
+        bookRepository.deleteById(id);
+    }
 }
