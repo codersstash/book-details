c74d1df6472f1ebe8c85a974d48ef224bae8d97e
codersstash <codersstashgitrepo@gmail.com>
2025-02-21 19:47:57 +0530

    Book Details System

--- src/main/java/com/codersstash/book_details/controller/BookController.java
+++ src/main/java/com/codersstash/book_details/controller/BookController.java
@@ -3,14 +3,21 @@ package com.codersstash.book_details.controller;
 import com.codersstash.book_details.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BookController {
-    @Autowired
-    private BookService bookService;
 
+    private final BookService bookService;
+
+    @Autowired
     public BookController(BookService bookService) {
         this.bookService = bookService;
+    }
+    @GetMapping("/index")
+    public String showAllBooks(Model model){
+        return "index";
     }
 
 }
