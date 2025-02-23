77df1df04bc22bcf381ef62ef7c200df0c10f7ad
codersstash <codersstashgitrepo@gmail.com>
2025-02-23 13:25:19 +0530

    Save New Book Details

--- src/main/java/com/codersstash/book_details/controller/BookController.java
+++ src/main/java/com/codersstash/book_details/controller/BookController.java
@@ -1,10 +1,13 @@
 package com.codersstash.book_details.controller;
 
+import com.codersstash.book_details.entity.Book;
 import com.codersstash.book_details.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -19,5 +22,15 @@ public class BookController {
         model.addAttribute("books", bookService.getAllBooks());
         return "index";
     }
-
+    @GetMapping("/addnewbook")
+    public String getNewBookForm(Model model){
+        Book book = new Book();
+        model.addAttribute("book", book);
+        return "addNewBookForm";
+    }
+    @PostMapping("/saveBook")
+    public String addNewBookForm(@ModelAttribute("book") Book book){
+        bookService.saveBook(book);
+        return "redirect:/index";
+    }
 }
